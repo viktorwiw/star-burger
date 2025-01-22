c4642689828948f9b40eb1fcecc893be53019bea
Shishkin PRO <viktorshish@gmail.com>
2025-01-22 15:21:11 +0300

    Returned the manager to the orders page after editing

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from django.shortcuts import reverse
+from django.shortcuts import reverse, redirect
 from django.templatetags.static import static
 from django.utils.html import format_html
+from django.utils.http import url_has_allowed_host_and_scheme
 
 from .models import Product, OrderDetails
 from .models import ProductCategory
@@ -118,6 +119,7 @@ class OrderAdmin(admin.ModelAdmin):
     fields = ['address', 'firstname', 'lastname', 'phonenumber']
     inlines = [OrderDetailsInline]
 
+
     def save_formset(self, request, form, formset, change):
         instances = formset.save(commit=False)
 
@@ -129,3 +131,15 @@ class OrderAdmin(admin.ModelAdmin):
             instance.save()
 
         formset.save_m2m()
+
+
+    def response_change(self, request, obj):
+        next_url = request.GET.get('next')
+        if "next" in request.GET and url_has_allowed_host_and_scheme(
+            url=next_url,
+            allowed_hosts={request.get_host()},
+            require_https=request.is_secure(),
+        ):
+            return redirect(next_url)
+
+        return super(OrderAdmin, self).response_change(request, obj)
